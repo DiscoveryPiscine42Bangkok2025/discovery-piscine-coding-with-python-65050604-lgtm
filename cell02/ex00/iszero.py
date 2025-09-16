cd190da81afc0090b028dd197736707dccf23f7c
65050604-lgtm <65050604@kmitl.ac.th>
2025-09-16 13:10:52 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,8 @@
+user_input = input("Enter a number: ")
+
+number = int(user_input)
+
+if number == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
