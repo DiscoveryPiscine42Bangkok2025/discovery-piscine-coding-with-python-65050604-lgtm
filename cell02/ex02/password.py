7eb11e646c71c7d13da491da0851d7b7b653433f
65050604-lgtm <65050604@kmitl.ac.th>
2025-09-16 13:16:18 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+correct_password = "Python is awesome"
+
+user_input_password = input("Enter password: ")
+
+if user_input_password == correct_password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
